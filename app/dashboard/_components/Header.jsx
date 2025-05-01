d731617d5eaf49d920081944e0178d3b452ec98c
Krishvora22 <krishvora22@gmail.com>
2025-05-01 08:43:08 +0530

    file update

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { UserButton } from '@clerk/nextjs'
+import { UserButton, useUser } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
@@ -12,9 +12,12 @@ function Header() {
     console.log(path)
   }, [])
 
+  const { isSignedIn } = useUser(); // Check if user is logged in
+
   return (
     <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
       <Image src={'/logo.svg'} width={65} height={75} alt='logo' />
+      {/* <div> */}
       <ul className='hidden md:flex gap-6'>
         <Link href={"/dashboard"}>
           <li className={`hover:text-primary hover:font-bold transition-all
@@ -24,14 +27,13 @@ function Header() {
 
           >Dashboard</li>
         </Link>
-
         <Link href={"/dashboard/upgrade"}>
           <li className={`hover:text-primary hover:font-bold transition-all
             cursor-pointer
             ${path == '/dashboard/upgrade' && 'text-primary font-bold'}
             `}>Upgrade</li>
         </Link>
-        <Link  href={"/dashboard/contact"}>
+        <Link href={"/dashboard/contact"}>
           <li className={`hover:text-primary hover:font-bold transition-all
             cursor-pointer
             ${path == '/dashboard/contact' && 'text-primary font-bold'}
@@ -40,9 +42,39 @@ function Header() {
           <li className={`hover:text-primary hover:font-bold transition-all
             cursor-pointer
             ${path == '/dashboard/about' && 'text-primary font-bold'}
-            `}>About Us</li></Link>
+            `}>About Us</li>
+          </Link>
       </ul>
+        {/* <Link href={"/dashboard/about"}>
+
+          <button className={` text-white bg-blue-600 hover:bg-blue-700 rounded-lg text-lg font-semibold shadow-md hover:shadow-lg transition text-center 
+            cursor-pointer
+            ${path == '/dashboard/about' && 'text-primary font-bold'}
+            `}>Login In</button></Link> */}
+            
+      {/* </div> */}
+
+      <nav>
+      {!isSignedIn && (
+        <div className="flex gap-4">
+          <a
+            href="/sign-in"
+            className={`text-white bg-blue-600 hover:bg-blue-700 rounded-lg text-lg font-semibold shadow-md hover:shadow-lg transition text-center px-4 py-2`}
+          >
+            Login
+          </a>
+          <a
+            href="/sign-up"
+            className={`text-white bg-blue-600 hover:bg-blue-700 rounded-lg text-lg font-semibold shadow-md hover:shadow-lg transition text-center px-4 py-2`}
+          >
+            Sign Up
+          </a>
+        </div>
+      )}
       <UserButton />
+      </nav>
+
+      
     </div>
   )
 }
